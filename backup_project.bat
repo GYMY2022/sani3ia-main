ec9733bed30f068ee0683f8985a05429dd52fd28
Gamal <gdrgcd66@gmail.com>
2026-02-24 23:01:16 +0200

    Auto Backup Tue 02/24/2026 23:01:16.21

--- backup_project.bat
+++ backup_project.bat
@@ -1,8 +1,22 @@
 @echo off
-cd /d "D:\sani3ia" 
+cd /d %~dp0
+
+echo ================================
+echo      SANI3IA AUTO BACKUP
+echo ================================
+
 git add .
-git commit -m "Auto backup - %date% %time%"
-git push
-echo ==============================
-echo ✅ Backup completed successfully!
+
+git diff --cached --quiet
+if %errorlevel%==0 (
+    echo No changes detected. Backup not needed.
+    pause
+    exit
+)
+
+git commit -m "Auto Backup %date% %time%"
+git push origin main
+
+echo.
+echo Backup Completed Successfully!
 pause
